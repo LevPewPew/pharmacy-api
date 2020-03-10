3d6ab2e8ae5cce7cba019a65334c420c826230fc
LevPewPew <levthedev@protonmail.com>
2020-03-10 16:49:16 +1100

    created a medicine class to use as a model

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
 import static spark.Spark.*;
 
+import org.apache.log4j.BasicConfigurator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+//        BasicConfigurator.configure();
 
         get("/hello", (req, res) -> {
             System.out.println("hello world!!!!!");
@@ -10,11 +19,27 @@ public class Main {
 
         get("/medication", (req, res) -> {
             System.out.println("here is some medication info");
+
+            JSONObject employeeDetails = new JSONObject();
+            employeeDetails.put("firstName", "Lokesh");
+            employeeDetails.put("lastName", "Gupta");
+            JSONArray employeeList = new JSONArray();
+            employeeList.add(employeeDetails);
+
+            try (FileWriter file = new FileWriter("employees.json")) {
+                file.write(employeeList.toJSONString());
+                file.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             return "here is some medication info";
         });
 
         post("/medication", (req, res) -> {
             System.out.println("thank you for this medication info");
+            System.out.println(req.body());
+            System.out.println(req.queryParams("medicationStrings"));
             return "thank you for this medication info";
         });
     }
---
+++ src/main/java/Medicine.java
@@ -0,0 +1,36 @@
+class Medicine {
+    private String id;
+    private String bottleSize;
+    private int dosageCount;
+
+    public Medicine(String id, String bottleSize, int dosageCount) {
+        super();
+        this.id = id;
+        this.bottleSize = bottleSize;
+        this.dosageCount = dosageCount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBottleSize() {
+        return bottleSize;
+    }
+
+    public void setBottleSize(String bottleSize) {
+        this.bottleSize = bottleSize;
+    }
+
+    public int getDosageCount() {
+        return dosageCount;
+    }
+
+    public void setDosageCount(int dosageCount) {
+        this.dosageCount = dosageCount;
+    }
+}
