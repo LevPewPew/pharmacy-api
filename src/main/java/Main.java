5d8af8f4308cc9650c7a58e615cd89dd42e924bf
LevPewPew <levthedev@protonmail.com>
2020-03-10 22:24:47 +1100

    getting and posting with a very simple form of medicationsStrings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import static spark.Spark.post;
 
 import com.google.gson.Gson;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 //        BasicConfigurator.configure();
@@ -20,15 +23,20 @@ public class Main {
 
             return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(medicationService.getMedicine(request.params(":id")))));
         });
-        
+
         post("/medications", (request, response) -> {
             response.type("application/json");
             System.out.println(request.body());
 
-//            Gson testGson = new Gson().fromJson(request.body(), );
-//            System.out.println(testGson);
+            MedicationsStrings medicationsStrings = new Gson().fromJson(request.body(), MedicationsStrings.class);
 
-            Medication medication = new Gson().fromJson(request.body(), Medication.class);
+            String[] medicationAttributes = medicationsStrings.getMedicationsStrings().split("_");
+            String id = medicationAttributes[0];
+            String bottleSize = medicationAttributes[1];
+            int dosageCount = Integer.parseInt(medicationAttributes[2]);
+
+            String json = String.format("{\"id\":\"%s\", \"bottleSize\": \"%s\", \"dosageCount\": %s}", id, bottleSize, dosageCount);
+            Medication medication = new Gson().fromJson(json, Medication.class);
             medicationService.addMedicine(medication);
 
             return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS));
---
+++ src/main/java/MedicationsStrings.java
@@ -0,0 +1,17 @@
+class MedicationsStrings {
+    private String medicationsStrings;
+
+    public MedicationsStrings(String medicationsStrings) {
+        super();
+
+        this.medicationsStrings = medicationsStrings;
+    }
+
+    public String getMedicationsStrings() {
+        return medicationsStrings;
+    }
+
+    public void setMedicationsStrings(String medicationsStrings) {
+        this.medicationsStrings = medicationsStrings;
+    }
+}
