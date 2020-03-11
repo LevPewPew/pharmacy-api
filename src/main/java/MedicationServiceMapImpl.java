77cae617b9019dfd0834302743ad8aae7a80adbd
LevPewPew <levthedev@protonmail.com>
2020-03-11 20:09:22 +1100

    returning the total meds and total dosages

--- src/main/java/MedicationServiceMapImpl.java
+++ src/main/java/MedicationServiceMapImpl.java
@@ -23,26 +23,31 @@ public class MedicationServiceMapImpl implements MedicationService {
         return medicationMap.get(id);
     }
 
+    // in this method i have assumed that "the total number of medications that have been inputted" refers to total
+    // amount of medicine strings that have been posted (i.e. total amount of Medicine objects created), not the total
+    // amount of unique medicationIds.
     @Override
     public HashMap<String, Integer> getStatistics() {
         HashMap<String, Integer> statistics = new HashMap<String, Integer>();
-        int totalMeds;
-        int totalDosages;
-        HashMap<String, Integer> totalMedsByBottleSize;
-        HashMap<String, Integer> perMedsSupplyCount;
+
+        int totalMeds = 0;
+        int totalDosages = 0;
+        HashMap<String, Integer> totalMedsByBottleSize = new HashMap<String, Integer>();
+        HashMap<String, Integer> perMedsSupplyCount = new HashMap<String, Integer>();;
 
         for (Medication medication : medicationMap.values()) {
-
+            totalMeds += 1;
+            totalDosages += medication.getDosageCount();
             System.out.println("```````````````````````````````");
             System.out.println(medication.getId());
+            System.out.println(medication.getMedicationId());
             System.out.println(medication.getBottleSize());
             System.out.println(medication.getDosageCount());
             System.out.println("_______________________________");
-
         }
 
-        statistics.put("foo", 123);
-        statistics.put("bar", 789);
+        statistics.put("totalMeds", totalMeds);
+        statistics.put("totalDosages", totalDosages);
 
         return statistics;
     }
