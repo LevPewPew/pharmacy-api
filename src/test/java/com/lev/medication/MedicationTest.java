0315652139fffaa3446f41983ff0566fe7810f48
LevPewPew <levthedev@protonmail.com>
2020-03-12 08:33:26 +1100

    basic unit test running

--- src/test/java/com/lev/medication/MedicationTest.java
+++ src/test/java/com/lev/medication/MedicationTest.java
@@ -1,7 +1,16 @@
 package com.lev.medication;
 
-import com.lev.medication.Medication;
+//import com.lev.medication.Medication;
 
+import com.lev.medication.Medication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 public class MedicationTest {
-    Medication.
+    @Test
+    public void testMe() {
+        Medication.validate(new Medication(1, "123abc", "S", 1000));
+    }
 }
